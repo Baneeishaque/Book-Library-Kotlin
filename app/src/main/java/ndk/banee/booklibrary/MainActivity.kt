d416f9b88043b76c28d31f9741b3b4210a1855ea
Banee Ishaque K <baneeishaque@gmail.com>
2020-07-24 16:36:35 +0530

    remove rxjava for room

--- app/src/main/java/ndk/banee/booklibrary/MainActivity.kt
+++ app/src/main/java/ndk/banee/booklibrary/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
@@ -18,41 +18,52 @@ class MainActivity : AppCompatActivity() {
         val database: BookLibraryDatabase =
             Room.databaseBuilder(this, BookLibraryDatabase::class.java, applicationName).build()
 
-        var id: Long
-        // For inserting the author
-        Observable.just("Author Name")
+        val authorId: Long
+        val insertAuthorSingle = Single.just("Author Name")
             .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
+            .observeOn(Schedulers.io())
             .subscribe({ authorName ->
-                id = database.authorDao().insert(AuthorModel(authorName = authorName))
-                Log.d(applicationName, "Author Inserted, Author ID : $id")
-            }, { throwable ->
-                Log.d(applicationName, "Error : ${throwable.printStackTrace()}")
-            }, {})
 
-//        // Author with Id
-//        Observable.just(database.authorDao().getAuthorWithId(id))
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe({
-//                Log.d(applicationName, "Author With id $id : ${it.toString()}")
-//            }, {
-//                Log.d(applicationName, "Error : ${it.printStackTrace()}")
-//            })
-//
-//        // Get all authors
-//        Observable.just(database.authorDao().getAllAuthors())
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe({
-//                val authorsIterator = it.listIterator()
-//                Log.d(applicationName, "Current Authors...")
-//                while (authorsIterator.hasNext()) {
-//
-//                    Log.d(applicationName, authorsIterator.next().toString())
-//                }
-//            }, {
-//                Log.d(applicationName, "Error : ${it.printStackTrace()}")
-//            })
+                authorId = database.authorDao().insert(AuthorModel(authorName = authorName))
+                Log.d(applicationName, "Author Inserted, Author ID : $authorId")
+
+            }, { throwable ->
+                Log.d(applicationName, "Error : ${throwable.localizedMessage}")
+            })
+
+        // Author with Id
+        val getAuthorSingle= Single.just(authorId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe({ localAuthorId ->
+                    Log.d(
+                        applicationName,
+                        "Author With id $localAuthorId : ${database.authorDao()
+                            .getAuthorWithId(localAuthorId)
+                            .toString()}"
+                    )
+
+                    // Get all authors
+                    Completable.complete()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(Schedulers.io())
+                        .subscribe({
+
+                            val authorList: List<AuthorModel> =
+                                database.authorDao().getAllAuthors()
+                            val authorsIterator = authorList.listIterator()
+                            Log.d(applicationName, "Current Authors...")
+                            while (authorsIterator.hasNext()) {
+
+                                Log.d(applicationName, authorsIterator.next().toString())
+                            }
+
+                        }, { throwable ->
+                            Log.d(applicationName, "Error : ${throwable.localizedMessage}")
+                        })
+
+                }, { throwable ->
+                    Log.d(applicationName, "Error : ${throwable.localizedMessage}")
+                })
     }
 }
